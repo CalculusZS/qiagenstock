7775ea73174605626cb1d00c565c19dd8cc51c5c
CalculusZS <frank.northbangkok@gmail.com>
2026-01-30 15:18:12 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /* ===== Config ===== */
-const API = "https://script.google.com/macros/s/AKfycby4m_wFWBohU0AwbXDeY6c_GMxiDUR94GOCIS69ERKMIQz-L-stoOUUtwYcYokDG4Ho4g/exec"; // e.g. https://script.google.com/macros/s/AKfy.../exec
+const API = "https://script.google.com/macros/s/AKfycbzdtc59bH-AN_d_TWMN06P428TlbtHdGpgiOVOI4SqpacJWZDFGCxZBxdwjVLOTKfLpbg/exec"; // e.g. https://script.google.com/macros/s/AKfy.../exec
 const PASSWORD = "Service";
 const SUP_PASSWORD = "Qiagen";
 
