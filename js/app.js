bf61e12d5529b7829f7c5f72a75167842f8cd0f9
CalculusZS <frank.northbangkok@gmail.com>
2026-02-26 21:49:28 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /* ========================================================================== 
    QIAGEN INVENTORY - FULL VERSION (GROUPED ITEMS & OUTLOOK FORCED)
    ========================================================================== */
-const API = "https://script.google.com/macros/s/AKfycbw1C4ctbbYEJMm9UCSzrdoKZ8Yfy0_e9BMqZFQ2qpvbRwAKhqewlJj2kLIX0X4RXQCb/exec";
+const API = "https://script.google.com/macros/s/AKfycbzG1H23irpdroTLl5VwRUpbjmXxzotzvy1v6IcoElH5u6yBYe2vo9DaHCsRL5jKmKWU/exec";
 const MASTER_PASS = "Service";
 const USER_MAP = {'KM':'Kitti','TK':'Tatchai','PSO':'Parinyachat','PK':'Phurilap','PST':'Penporn','PA':'Phuriwat'};
 
